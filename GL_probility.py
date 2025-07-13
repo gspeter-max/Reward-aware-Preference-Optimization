e5609bcac5135e38f916514e2ec937ab1d636c7f
gspeter-max <gspeter67@gmail.com>
2025-07-13 15:08:18 +0530

    update

--- GL_probility.py
+++ GL_probility.py
@@ -3,24 +3,41 @@ import torch
 class Generate_Response_Prob:
     def __init__(
             self,
-            tokenizer
+            tokenizer, 
+            num_responses_for_one_prompt= 1
+            batched_response = 1 
             ):
-        self.tokenizer = tokenizer 
+        self.tokenizer = tokenizer
+        self.num_responses = num_responses_for_one_prompt
+        self.batched_response = batched_response 
 
     def compute_prob( self,model, prompt, generated_response_k ):
         log_prob_list = []
         log_prob = 0
-        for generated_response in generated_response_k:
-            response_tokenized = self.tokenizer( generated_response,return_tensor = 'pt' )
+        for i in range( self.num_responses ):
+            generated_response = generated_response_k[i : i+1 ].squeeze(0)
 
-            for index , tokenized_ids in enumerate(response_tokenized.input_ids):
-                prompt_tokenized = self.tokenizer( prompt, return_tensors = 'pt' ) 
-
+            response_tokenized = self.tokenizer(
+                    generated_response,
+                    return_tensors = 'pt', 
+                    truncation = True,
+                    padding = 'max_length'
+                            ) 
+            
+            for j in range( tokenizer.max_length_single_sentance):
+                prompt_tokenized = self.tokenizer( 
+                                                  prompt,
+                                                  return_tensors = 'pt', 
+                                                  truncation = True, 
+                                                  padding = 'max_length'
+                                                  ) 
+                
                 model_output = model( **prompt_tokenized ).logits 
 
                 soft_max_model_output = torch.nn.functional.softmax(model_output[:,-1,:], dim = -1)
-                log_prob = log_prob + torch.log(soft_max_model_output[ :,tokenized_ids])
+                log_prob = log_prob + torch.log(soft_max_model_output[ :,response_tokenized[:,:, j ] ] 
                 prompt = prompt + self.tokenizer.decode( tokenized_ids )
+
             log_prob_list.append( log_prob ) 
         
         return torch.tensor(
---
+++ sudo_codes.py
@@ -0,0 +1,6 @@
+
+a = [2,3,4,2] 
+print( a[3:4] ) 
+
+#for i in range(num_respose):
+ #   responses = batched_response[
