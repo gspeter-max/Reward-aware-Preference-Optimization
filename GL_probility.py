58b4c04433a062a354fe15c3a7e5c2b7ccae9e78
gspeter-max <gspeter67@gmail.com>
2025-07-13 12:54:18 +0530

    update

--- GL_probility.py
+++ GL_probility.py
@@ -8,18 +8,21 @@ class Generate_Response_Prob:
         self.tokenizer = tokenizer 
 
     def compute_prob( self,model, prompt, generated_response_k ):
-        log_prob_list = [] 
+        log_prob_list = []
         log_prob = 0
         for generated_response in generated_response_k:
-            response_tokenized = tokenizer( generated_response )
+            response_tokenized = self.tokenizer( generated_response,return_tensor = 'pt' )
+
             for index , tokenized_ids in enumerate(response_tokenized.input_ids):
-                prompt_tokenized = self.tokenizer( prompt, return_tensors = 'pt' )
+                prompt_tokenized = self.tokenizer( prompt, return_tensors = 'pt' ) 
+
                 model_output = model( **prompt_tokenized ).logits 
+
                 soft_max_model_output = torch.nn.functional.softmax(model_output[:,-1,:], dim = -1)
                 log_prob = log_prob + torch.log(soft_max_model_output[ :,tokenized_ids])
                 prompt = prompt + self.tokenizer.decode( tokenized_ids )
             log_prob_list.append( log_prob ) 
         
-        return log_prob_list
+        return torch.tensor( log_prob_list ) 
 
 
--- rpo_loss.py
+++ rpo_loss.py
@@ -1,47 +1,7 @@
 import torch 
-from GL_probility import G
-class rpo_loss_func:
-    def __init__( self, ref_model , policy_model , beta, distance_matrix ,reward_scaling ):
-        self.ref_model = ref_model 
-        self.policy_model = policy_model 
-        self.beta = beta 
-        self.D = distance_matrix 
-        self.reward_scaling = reward_scaling 
-    
-    def backward_kl( self,y_k,x, reward_k_response: list ):
-        '''
-        first we are need to compute π(yk∣x) likehood of the model to generate a prompt 
-        that is done by computing the sum of all the token probability that have in respone 
-        '''
-
-        raw_reward_prob = self.reward_scaling * reward_k_response
-        raw_policy_likelihood = get_model_prob.compute_prob(
-                self.policy_model, 
-                x, 
-                y_k
-            )
-        raw_ref_model_likelihood = get_model_prob.compute_prob(
-                self.ref_model, 
-                x, 
-                y_k 
-            ) 
-        raw_models_prob = [ self.beta * torch.log( rpm / rfm ) 
-                for rpm , rfm in zip( raw_policy_likelihood , raw_ref_model_likelihood ) 
-                ]
-        model_reward_prob = torch.nn.functional.softmax( raw_models_prob, dim = -1 ) 
-        reward_model_prob = torch.nn.functional.softmax( raw_reward_prob, dim = -1 )
-        
-        loss = [rmp * torch.log( rmp / mrp ) for rmp, mrp in zip( reward_model_prob, model_reward_prob)]
-        return torch.sum( loss, dim = -1)
-
-    def __call__(self , 
-
-
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 from transformers.utils.quantization_config import BitsAndBytesConfig
-import torch
 
 quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -50,11 +10,11 @@ quantization_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=True
 )
 
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium", use_fast = True)
-tokenizer.pad_token = tokenizer.eos_token
+tokenizer = AutoTokenizer.from_pretrained("gpt2", use_fast = True)
+tokenizer.pad_token = tokenizer.eos_token 
 
 model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/DialoGPT-medium",
+    "gpt2",
     quantization_config=quantization_config
 
     )
@@ -74,3 +34,54 @@ dataset.set_format(
     columns = ['input_ids','attention_mask', 'labels'], 
     output_all_columns = True 
 )
+
+get_model_prob = Generate_Response_Prob( tokenizer )
+class rpo_loss_func:
+    def __init__( self, ref_model= model, policy_model = model , beta = 0.3, distance_matrix= None ,reward_scaling = 0.3 ):
+        self.ref_model = ref_model 
+        self.policy_model = policy_model 
+        self.beta = beta 
+        self.D = distance_matrix 
+        self.reward_scaling = torch.tensor(reward_scaling)  
+    
+    def backward_kl( self,y_k,x, reward_k_response):
+        '''
+        first we are need to compute π(yk∣x) likehood of the model to generate a prompt 
+        that is done by computing the sum of all the token probability that have in respone 
+        '''
+        #if not isinstance( reward_k_response, torch.Tensor ):
+            #            reward_k_response = torch.tensor(reward_k_response)
+
+        raw_reward_prob = self.reward_scaling * reward_k_response
+        raw_policy_likelihood = get_model_prob.compute_prob(
+                self.policy_model, 
+                x, 
+                y_k
+            )
+        raw_ref_model_likelihood = get_model_prob.compute_prob(
+                self.ref_model, 
+                x, 
+                y_k 
+            ) 
+#        raw_models_prob = torch.tensor([ self.beta * torch.log( rpm / rfm ) 
+                                        #                for rpm , rfm in zip( raw_policy_likelihood , raw_ref_model_likelihood ) 
+                                        #       ]) 
+        raw_models_prob = self.beta * torch.log( raw_policy_likelihood / rew_ref_model_likelihood )
+        model_reward_prob = torch.nn.functional.softmax( raw_models_prob, dim = -1 )
+        reward_model_prob = torch.nn.functional.softmax( raw_reward_prob, dim = -1 )
+        
+#        loss = torch.tensor([rmp * torch.log( rmp / mrp ) for rmp, mrp in zip( reward_model_prob, model_reward_prob)]) 
+        loss = reward_model_prob * torch.log( reward_model_prob/ model_reward_prob ) 
+        return torch.sum( loss, dim = -1)
+
+loss_func = rpo_loss_func()
+
+x = 'hi i am genius i am extremly creative? '
+y = ['yeah i am ', 'i think yes']
+z = torch.tensor([0.234,0.43]) 
+
+loss_func.backward_kl( y, x, z)
+
+    # def __call__(self ,
+# from GL_probility import G
+
