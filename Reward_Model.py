5bfccad3eb2c8dd541344d397ef2f26eb40c5aa2
gspeter-max <gspeter67@gmail.com>
2025-07-18 16:13:42 +0530

    reward model is completely done

--- Reward_Model.py
+++ Reward_Model.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForCausalLM  ,AutoTokenizer 
-from torch.utils.data import DataLoader, TensorData
+from torch.utils.data import DataLoader, TensorDataset
 import torch.nn as nn
 import torch
 from datasets import load_dataset
@@ -20,34 +20,43 @@ class Reward_Model(nn.Module):
         self.hidden = None 
 
     def forward(self, x):
-        hidden_state = self.model(**x).logits[:, -1,:]
+        hidden_state = self.model(
+            input_ids= x[0], 
+            attention_mask = x[1]
+        ).logits[:, -1,:]
         self.hidden = hidden_state 
         return self.final_layer(hidden_state ) 
 
 reward_model = Reward_Model( model = model, model_output_shape = model.config.vocab_size ).to('cuda')
 data = load_dataset('Anthropic/hh-rlhf')['test'] 
 
-batch_size = 300
-if tokenized_data.input_ids.shape[0] % 3 != 0: 
-    RuntimeError('batch size is not correct') 
 
 data = data.to_pandas().astype(str).values.flatten().tolist()
 tokenized_data = tokenizer(data, truncation= True, padding = 'max_length', return_tensors = 'pt').to('cuda')
 dataset = TensorDataset(tokenized_data.input_ids,tokenized_data.attention_mask)
 
-data = DataLoader(dataset, batch_size = batch_size)
-optim = torch.optim.Adam( reward_model.Parameters() , lr = 0.00234 )
 
-for index, batched_data in enumerate(dataset):
-    reward_scores = reward_model(
-            input_ids = batched_data[0], 
-            attention_mask= batched_data[1]
-            ) 
+batch_size = 8
+print(tokenized_data.input_ids.shape[0]);print( batch_size )
+if (tokenized_data.input_ids.shape[0] % batch_size) != 0: 
+    raise ValueError('batch size is not correct') 
+
+data = DataLoader(dataset, batch_size = batch_size)
+optim = torch.optim.Adam( reward_model.parameters() , lr = 0.00234 )
+
+for index, batched_data in enumerate(data):
+    print(len(batched_data))
+    if index == 8:
+        break 
+    reward_scores = reward_model(batched_data)
     y1 = reward_scores[0::2]
     y2 = reward_scores[1::2]
     x = y1 - y2
-    loss = -torch.nn.functional.log_sigmoid(x) 
+    loss = -torch.nn.functional.logsigmoid(x)
+    loss = loss.mean()
     loss.backward()
     optim.step()
     optim.zero_grad()
+    print(f'loss is : {loss}')
+
 
